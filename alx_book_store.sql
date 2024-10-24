81574db05c62e5f6ceadf5fea38811ad19fde650
njoga lawrence <njogalawrence@gmail.com>
2024-10-24 11:55:40 +0300

    Add alx_book_store.sql

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -4,24 +4,24 @@ CREATE DATABASE IF NOT EXISTS alx_book_store;
 USE alx_book_store;
 
 --CREATE TABLES
-CREATE TABLE Books(book_id INT AUTO_INCREMENT Primary Key,
+CREATE TABLE Books(book_id INT AUTO_INCREMENT PRIMARY KEY,
 title VARCHAR(130),
-author_id INT Foreign Key referencing Authors table,
+author_id INT FOREIGN KEY referencing Authors table,
 price DOUBLE NOT NULL,
 publication_date DATE);
 
 CREATE TABLE Authors(author_id INT Primary Key AUTO_INCREMENT,
 author_name VARCHAR(215));
 
-CREATE TABLES Customers(customer_id INT Primary Key AUTO_INCREMENT,
+CREATE TABLES Customers(customer_id INT PRIMARY KEY AUTO_INCREMENT,
 customer_name VARCHAR(215),
 email VARCHAR(215), address TEXT);
 
-CREATE TABLE Orders(order_id INT Primary Key AUTO_INCREMENT,
-customer_id Foreign Key referencing Customers table,
+CREATE TABLE Orders(order_id INT PRIMARY KEY AUTO_INCREMENT,
+customer_id FOREIGN KEY referencing Customers table,
 order_date DATE);
 
-CREATE TABLE Order_Details(orderdetailid INT Primary Key AUTO_INCREMENT,
-order_id INT Foreign Key referencing Orders table,
-book_id INT Foreign Key referencing Books table,
+CREATE TABLE Order_Details(orderdetailid INT PRIMARY KEY AUTO_INCREMENT,
+order_id INT FOREIGN KEY referencing Orders table,
+book_id INT FOREIGN KEY referencing Books table,
 quantity DOUBLE);
