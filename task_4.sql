bc36439b2e5aefd8743db344aeff4bebd345c957
njoga lawrence <njogalawrence@gmail.com>
2024-10-24 14:08:30 +0300

    Modify task4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,2 +1,15 @@
 USE alx_book_store;
-SELECT * FROM Books
+-- Query to get the full description of the Books table
+SELECT 
+    COLUMN_NAME,
+    DATA_TYPE,
+    CHARACTER_MAXIMUM_LENGTH,
+    IS_NULLABLE,
+    COLUMN_DEFAULT,
+    COLUMN_KEY,
+    EXTRA
+FROM 
+    information_schema.COLUMNS
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store' AND 
+    TABLE_NAME = 'Books';
