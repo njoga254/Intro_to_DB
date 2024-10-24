9e81a87ec6fe95a6d6229660b113f5aed280b43f
njoga lawrence <njogalawrence@gmail.com>
2024-10-24 13:45:58 +0300

    Add task_6.sql

--- task_6.sql
+++ task_6.sql
@@ -1,1 +1,1 @@
-INSERT INTO customers(customer_id, customer_name, email, address) VALUES (2, "Blessing Malik", "bmalik@sandtech.com", "124 Happiness Ave."), (3, "Obed Ehoneah", "eobed@sandtech.com", "125 Happiness Ave."),(4, "Nehemiah Kamolu", "nkamolu@sandtech.com", "126 Happiness Ave.") 
+INSERT INTO Customers(customer_id, customer_name, email, address) VALUES (, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'), (, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),(, 'Nehemiah Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.'); 
