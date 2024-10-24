a359aa56949566eb9128b68022d2b4ebdb47501f
njoga lawrence <njogalawrence@gmail.com>
2024-10-24 19:00:15 +0300

    Modify task6.sql

--- task_6.sql
+++ task_6.sql
@@ -1,6 +1,6 @@
 INSERT INTO customers (customer_name, email, address)
 VALUES 
 ('Blessing Malik', 'bmalik@sandtech.com', '124 Happiness  Ave.'),
-('Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
-('Nehemiah Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
+('Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness  Ave.'),
+('Nehemiah Kamolu', 'nkamolu@sandtech.com', '126 Happiness  Ave.');
  
