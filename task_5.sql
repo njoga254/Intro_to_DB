32f08975feb43ab83989607b090e7ace11993d3a
njoga lawrence <njogalawrence@gmail.com>
2024-10-24 14:13:33 +0300

    Modify task5.sql

--- task_5.sql
+++ task_5.sql
@@ -1,1 +1,1 @@
-INSERT INTO Customers(customer_name, email, address) VALUES ('Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
+INSERT INTO Customers(customer_id,customer_name, email, address) VALUES ('Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
